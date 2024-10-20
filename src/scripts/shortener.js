d02f23020499d7735108fff7c87b9e350a827928
Farideh <heidarian89@gmail.com>
2024-10-20 09:26:23 +0330

    add urls

--- src/scripts/shortener.js
+++ src/scripts/shortener.js
@@ -18,3 +18,61 @@ form.addEventListener("submit", (e) => {
 
   save(aliasToOriginal);
 });
+
+// Alias link generator starts
+
+const alias_form = document.querySelector("#alias_link_form");
+// const aliasInput = document.getElementById("alias_input");
+// const linkInput = document.getElementById("link_input");
+
+alias_form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // const alias = aliasInput.value.trim();
+  // const link = linkInput.value.trim();
+
+  const formData = new FormData(e.target);
+  const link = formData.get("link");
+  const alias = formData.get("alias");
+
+  aliasToOriginal.set(alias, link);
+  displayUrl(aliasToOriginal);
+  save(aliasToOriginal);
+});
+
+// Alias link generator ends
+
+// Display URLs
+
+const displayUrl = () => {
+  const display_urls = document.getElementById("display_urls");
+  display_urls.innerHTML = "";
+
+  aliasToOriginal.forEach((link, alias) => {
+    const formHTML = `
+        <div class="card" id="displayUrlCard"> 
+        <form id="display_url_form">
+            <div class="form-group">
+                <input
+                    type="text"
+                    id="alias_display"
+                    name="alias"
+                   value="${alias}"
+                />
+                <label for="link_display">${link}</label>
+            </div> 
+            <div class="controlButtons">
+                <button type="button" class="confirm">Confirm</button>
+                <button type="button" class="copy">Copy</button>
+                <button type="button" class="delete">Delete</button>
+            </div>           
+        </form>
+        </div>
+    `;
+    display_urls.innerHTML += formHTML;
+  });
+};
+
+const documentReady = () => {
+  displayUrl();
+};
+document.addEventListener("DOMContentLoaded", documentReady);
