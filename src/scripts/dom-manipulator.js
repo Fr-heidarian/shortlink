be9b5b05115c83de164e6acf4ee2a902b80d9da2
Farideh <heidarian89@gmail.com>
2024-11-01 15:04:35 +0330

    [REFACTOR] refactor displayUrl for single responsibility

--- src/scripts/dom-manipulator.js
+++ src/scripts/dom-manipulator.js
@@ -9,31 +9,35 @@ export class DomManipulator {
     this.#displayUrl();
   }
 
+  #generateUrlCard(link, alias) {
+    return `
+    <div class="card">
+      <form>
+        <div class="form-group">
+          <input
+            type="text"
+            name="alias"
+            value="${alias}"
+          />
+          <label>${link}</label>
+        </div>
+        <div class="control-buttons">
+          <button type="button" class="confirm">Confirm</button>
+          <button type="button" class="copy">Copy</button>
+          <button type="button" class="delete">Delete</button>
+        </div>
+      </form>
+    </div>
+  `;
+  }
+
   #displayUrl() {
     const displayUrls = document.getElementById("display-urls");
     displayUrls.innerHTML = "";
 
     this.shortener.aliasToOriginal.forEach((link, alias) => {
-      const formHTML = `
-        <div class="card"> 
-        <form>
-            <div class="form-group">
-                <input
-                    type="text"
-                    name="alias"
-                   value="${alias}"
-                />
-                <label>${link}</label>
-            </div> 
-            <div class="control-buttons">
-                <button type="button" class="confirm">Confirm</button>
-                <button type="button" class="copy">Copy</button>
-                <button type="button" class="delete">Delete</button>
-            </div>           
-        </form>
-        </div>
-    `;
-      displayUrls.innerHTML += formHTML;
+      const urlCard = this.#generateUrlCard(link, alias);
+      displayUrls.innerHTML += urlCard;
     });
   }
 }
